76bc0a8bda91deac50d8daf0d4eb71863f3d307c
Archangel603 <mikhailmikhailoff@yandex.ru>
2021-11-29 22:06:04 +0300

    Fixed missing files

---
+++ java-app/src/main/java/ai/smartcat/connectortest/ConnectortestApplication.java
@@ -0,0 +1,13 @@
+package ai.smartcat.connectortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectortestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectortestApplication.class, args);
+	}
+
+}
---
+++ java-app/src/test/java/ai/smartcat/connectortest/ConnectortestApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.smartcat.connectortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectortestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
